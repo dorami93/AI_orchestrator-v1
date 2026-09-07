e4e8969388d2f8f91d7ca71c6826707e8f9c1e89
dorami93 <is0570vp@ed.ritsumei.ac.jp>
2026-09-08 01:32:49 +0900

    Implement markdown function for text formatting
    Added a markdown function to convert markdown syntax to HTML.

--- output.py
+++ output.py
@@ -1,4 +1,13 @@
+import re
 from js import document
+
+
+def markdown(text):
+    text = text.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+    text = re.sub(r"```(\w*)\n([\s\S]*?)```", r"<pre><code>\2</code></pre>", text)
+    text = re.sub(r"`([^`]+)`", r"<code>\1</code>", text)
+    text = re.sub(r"\*\*(.+?)\*\*", r"<b>\1</b>", text)
+    return text
 
 
 def render_messages(element, messages):
@@ -7,7 +16,7 @@ def render_messages(element, messages):
     for message in messages:
         div = document.createElement("div")
         div.className = "msg " + message["role"]
-        div.textContent = message["content"]
+        div.innerHTML = markdown(message["content"])
         element.appendChild(div)
 
     element.scrollTop = element.scrollHeight
